a272708bd51eb3420e5adbb165e24c6697b92a99
diva-sl <divakaran.sivasamy@gmail.com>
2025-05-18 18:32:10 +0530

    dshjskhjds

--- src/scripts/export-icons.tsx
+++ src/scripts/export-icons.tsx
@@ -1,75 +1,75 @@
-// src/scripts/export-icons.tsx
+// // src/scripts/export-icons.tsx
 
-import * as fs from 'fs'
-import * as path from 'path'
+// import * as fs from 'fs'
+// import * as path from 'path'
 
-import React from 'react'
-import ReactDOMServer from 'react-dom/server'
-import type { IconType } from 'react-icons'
+// import React from 'react'
+// import ReactDOMServer from 'react-dom/server'
+// import type { IconType } from 'react-icons'
 
-import {
-  FaSpa,
-  FaUtensils,
-  FaRunning,
-  FaCar,
-  FaGraduationCap,
-  FaHome,
-  FaCocktail,
-  FaCalendarAlt,
-  FaMoneyCheckAlt,
-  FaHeartbeat,
-  FaHotel,
-  FaNetworkWired,
-  FaLaptopCode,
-  FaBalanceScale,
-  FaPalette,
-  FaMusic,
-  FaPaw,
-  FaBuilding,
-  FaShoppingBag,
-  FaBriefcase,
-  FaTools,
-  FaDumbbell,
-  FaCameraRetro,
-  FaTruck,
-} from 'react-icons/fa'
+// import {
+//   FaSpa,
+//   FaUtensils,
+//   FaRunning,
+//   FaCar,
+//   FaGraduationCap,
+//   FaHome,
+//   FaCocktail,
+//   FaCalendarAlt,
+//   FaMoneyCheckAlt,
+//   FaHeartbeat,
+//   FaHotel,
+//   FaNetworkWired,
+//   FaLaptopCode,
+//   FaBalanceScale,
+//   FaPalette,
+//   FaMusic,
+//   FaPaw,
+//   FaBuilding,
+//   FaShoppingBag,
+//   FaBriefcase,
+//   FaTools,
+//   FaDumbbell,
+//   FaCameraRetro,
+//   FaTruck,
+// } from 'react-icons/fa'
 
-const icons: Record<string, IconType> = {
-  FaSpa,
-  FaUtensils,
-  FaRunning,
-  FaCar,
-  FaGraduationCap,
-  FaHome,
-  FaCocktail,
-  FaCalendarAlt,
-  FaMoneyCheckAlt,
-  FaHeartbeat,
-  FaHotel,
-  FaNetworkWired,
-  FaLaptopCode,
-  FaBalanceScale,
-  FaPalette,
-  FaMusic,
-  FaPaw,
-  FaBuilding,
-  FaShoppingBag,
-  FaBriefcase,
-  FaTools,
-  FaDumbbell,
-  FaCameraRetro,
-  FaTruck,
-}
+// const icons: Record<string, IconType> = {
+//   FaSpa,
+//   FaUtensils,
+//   FaRunning,
+//   FaCar,
+//   FaGraduationCap,
+//   FaHome,
+//   FaCocktail,
+//   FaCalendarAlt,
+//   FaMoneyCheckAlt,
+//   FaHeartbeat,
+//   FaHotel,
+//   FaNetworkWired,
+//   FaLaptopCode,
+//   FaBalanceScale,
+//   FaPalette,
+//   FaMusic,
+//   FaPaw,
+//   FaBuilding,
+//   FaShoppingBag,
+//   FaBriefcase,
+//   FaTools,
+//   FaDumbbell,
+//   FaCameraRetro,
+//   FaTruck,
+// }
 
-const exportDir = path.resolve(__dirname, '../public/icons')
+// const exportDir = path.resolve(__dirname, '../public/icons')
 
-if (!fs.existsSync(exportDir)) {
-  fs.mkdirSync(exportDir, { recursive: true })
-}
+// if (!fs.existsSync(exportDir)) {
+//   fs.mkdirSync(exportDir, { recursive: true })
+// }
 
-Object.entries(icons).forEach(([name, Icon]) => {
-  const svg = ReactDOMServer.renderToStaticMarkup(<Icon size={24} />)
-  fs.writeFileSync(path.join(exportDir, `${name}.svg`), svg)
-})
+// Object.entries(icons).forEach(([name, Icon]) => {
+//   const svg = ReactDOMServer.renderToStaticMarkup(<Icon size={24} />)
+//   fs.writeFileSync(path.join(exportDir, `${name}.svg`), svg)
+// })
 
-console.log('SVG icons exported!')
+// console.log('SVG icons exported!')
